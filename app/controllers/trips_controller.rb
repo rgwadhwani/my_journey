57da065ba65b8b2bd822e9ff9e74e7b46215be52
Raghu Betina <raghu@firstdraft.com>
2019-08-24 20:08:09 +0000

    Only trip user can access show page

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,5 +1,5 @@
 class TripsController < ApplicationController
-  before_action :current_user_must_be_trip_user, :only => [:edit_form, :update_row, :destroy_row]
+  before_action :current_user_must_be_trip_user, :only => [:show, :edit_form, :update_row, :destroy_row]
 
   def current_user_must_be_trip_user
     trip = Trip.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
