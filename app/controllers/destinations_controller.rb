e4e151f55f62520ff9a79f6acdc055cad430f4d3
Raghu Betina <raghu@firstdraft.com>
2019-08-24 20:08:22 +0000

    Install Kaminari

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,6 +1,6 @@
 class DestinationsController < ApplicationController
   def index
-    @destinations = Destination.all
+    @destinations = Destination.page(params[:page]).per(10)
 
     render("destination_templates/index.html.erb")
   end
